8fd16213372af5cd8292fbd33a0d807302f658f9
MikeBLambert <mlamb128@gmail.com>
2018-01-29 19:26:24 -0800

    add javascript allowing for user to input any number between 1 and 3,999 and return a roman numeral.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,47 @@
 //Business Logic
-function numberToRomanNumeral(a) {
-  var number = parseInt(a);
-  var numberDigits = [1,5,10,50,100,500,1000];
-  var romanDigits = ["I","V","X","L","C","D","M"];
-  for (var i=0; i <= numberDigits.length; i += 1) {
-    if (number === numberDigits[i]) {
-      return romanDigits[i];
+function ones(a) {
+  var onesNumerals = ["","I","II","III","IV","V","VI","VII","VIII","IX"];
+  var allNumbers = ["0","1","2","3","4","5","6","7","8","9"];
+  var padNumber = a.padStart(4,"0");
+  var arrayNumber = padNumber.split("");
+  for (var i=0; i<=allNumbers.length;i+=1) {
+    if (arrayNumber[3]===allNumbers[i]) {
+      return onesNumerals[i];
     };
   };
-  console.log("else");
+};
+function tens(a) {
+  var tensNumerals = ["","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"];
+  var allNumbers = ["0","1","2","3","4","5","6","7","8","9"];
+  var padNumber = a.padStart(4,"0");
+  var arrayNumber = padNumber.split("");
+    for (var i=0; i<=allNumbers.length;i+=1) {
+      if (arrayNumber[2]===allNumbers[i]) {
+        return tensNumerals[i];
+      };
+    };
+};
+function hundreds(a) {
+  var hundredsNumerals = ["","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"];
+  var allNumbers = ["0","1","2","3","4","5","6","7","8","9"];
+  var padNumber = a.padStart(4,"0");
+  var arrayNumber = padNumber.split("");
+    for (var i=0; i<=allNumbers.length;i+=1) {
+      if (arrayNumber[1]===allNumbers[i]) {
+        return hundredsNumerals[i];
+      };
+    };
+};
+function thousands(a) {
+  var thousandsNumerals = ["","M","MM","MMM"];
+  var allNumbers = ["0","1","2","3"];
+  var padNumber = a.padStart(4,"0");
+  var arrayNumber = padNumber.split("");
+    for (var i=0; i<=allNumbers.length;i+=1) {
+      if (arrayNumber[0]===allNumbers[i]) {
+        return thousandsNumerals[i];
+      };
+    };
 };
 
 //User Interface
@@ -16,7 +49,7 @@ $(document).ready(function() {
   $("#numberInput").submit(function(event) {
   event.preventDefault();
     var number = $("#input").val();
-    var romanNumeral = numberToRomanNumeral(number);
+    var romanNumeral = thousands(number)+hundreds(number)+tens(number)+ones(number);
     $("#output").html(romanNumeral);
   });
 });
